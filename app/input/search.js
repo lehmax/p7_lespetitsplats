3463193ce20cc4ddb38ca663276158777c5e24e5
Maxime Leherquer <45520008+lehmax@users.noreply.github.com>
2024-02-16 01:52:14 +0100

    add msg

--- app/input/search.js
+++ app/input/search.js
@@ -3,6 +3,19 @@ export const searchInput = () => {
   const inputSearch = formSearch.querySelector('input[type="search"]')
   const state = { value: '' }
 
+  const invalidMsg = `
+    <span
+      class="absolute
+      left-0 py-2 text-xs
+      font-bold text-white
+      border-2 rounded bg-rose-500
+      px-9 -bottom-12 invalid
+     border-rose-500"
+    >
+      Veuillez saisir au moins 3 caractères
+    </span>
+  `
+
   const onSearch = (event) => {
     event.preventDefault()
     const isSameSearch = inputSearch.value?.trim() === state.value
@@ -12,9 +25,16 @@ export const searchInput = () => {
     }
 
     state.value = inputSearch.value.trim()
+    formSearch.classList.remove('error')
+    if (inputSearch.nextElementSibling) {
+      inputSearch.nextElementSibling.remove()
+    }
 
     const isInvalid = state.value.length < 3
-    formSearch.classList.toggle('error', isInvalid)
+    if (isInvalid) {
+      formSearch.classList.add('error')
+      inputSearch.insertAdjacentHTML('afterend', invalidMsg)
+    }
 
     formSearch.dispatchEvent(
       new Event('searchRecipes', {
