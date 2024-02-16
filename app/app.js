659af3e55bbf12577f119c4bb7ecd28ee5ed6369
Maxime Leherquer <45520008+lehmax@users.noreply.github.com>
2024-02-16 01:26:26 +0100

    misc fix

--- app/app.js
+++ app/app.js
@@ -29,7 +29,7 @@ const app = (initRecipes) => {
     document.addEventListener('searchRecipes', onSearch)
   }
 
-  const onSearch = ({ detail }) => {
+  const onSearch = () => {
     toggleNoResultsMessage(false)
     search()
     updateRecipesList()
